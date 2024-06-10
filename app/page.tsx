3bbad11e46e46355fb548fd3d679585ffb1b7730
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-10 10:36:45 +0530

    Initialized the project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import {Test} from './components/test'
+export default function Home() {
+  return (
+  <>
+<div className="min-h-screen bg-zinc-900 h-full overflow-x-hidden">
+<Test className="text-white"/>
+
+</div>
+  </>
+  );
+}
